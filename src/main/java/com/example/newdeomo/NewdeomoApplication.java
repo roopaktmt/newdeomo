7ccc2f9b13a0971a1c8ca61f0d3c1d8d38b7114c
roopa <roopa.bhavik@gmail.com>
2023-12-11 16:32:47 -0500

    second commit

--- src/main/java/com/example/newdeomo/NewdeomoApplication.java
+++ src/main/java/com/example/newdeomo/NewdeomoApplication.java
@@ -2,10 +2,17 @@ package com.example.newdeomo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NewdeomoApplication {
-
+	
+@GetMapping("/")
+	public String sayHello() {
+		return "OM SHIV SHANKARAYANAMAH";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(NewdeomoApplication.class, args);
 	}
