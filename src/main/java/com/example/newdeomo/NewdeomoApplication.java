272d16a6d2509f6cedd5fd53588e4718e91a6fb0
roopa <roopa.bhavik@gmail.com>
2023-12-11 16:16:27 -0500

    fiurst commit

---
+++ src/main/java/com/example/newdeomo/NewdeomoApplication.java
@@ -0,0 +1,13 @@
+package com.example.newdeomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewdeomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewdeomoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newdeomo/NewdeomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newdeomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewdeomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
